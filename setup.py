9f6a53798810cc6ad488411509f0d5982af5c77d
Graham Poulter <graham.poulter@gmail.com>
2009-12-04 15:10:28 +0200

    [svn r22] Bumped setup.py version, and import ez_setup only if necessary.

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
 
-import ez_setup
-ez_setup.use_setuptools()
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
-    name='ngram',
-    version='3.0',
-    py_modules=['ngram'],
-    zip_safe=True,
-
-    author='Michael Albert, Graham Poulter',
-    author_email='skygreen@users.sourceforge.net',
-    license='LGPL',
-
-    url='http://www.sourceforge.net/projects/python-ngram',
-    description="A set class that can search its members by N-Gram similarity to a query string",
-    keywords="ngram string similarity",
+    name = 'ngram',
+    version = '3.1',
+    py_modules = ['ngram'],
+    zip_safe = True,
+    author = 'Michael Albert, Graham Poulter',
+    license = 'LGPL',
+    url = 'http://www.sourceforge.net/projects/python-ngram',
+    description = "A set that retrieves members by N-Gram similarity to a query string.",
+    keywords = "ngram string similarity",
 )
+
