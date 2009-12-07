056a98dab52afc7da7d61fec127998a40f165aa9
Graham Poulter <graham.poulter@gmail.com>
2009-12-07 13:15:43 +0200

    [svn r32] Changed from ez_setup to distribute_setup (replaces setuptools), and now support Python 3 via use_2to3 in setup.py.

--- setup.py
+++ setup.py
@@ -4,21 +4,29 @@ try:
     from setuptools import setup
 except ImportError:
     try:
-        import ez_setup
-        ez_setup.use_setuptools()
+        from distribute_setup import use_setuptools
+        use_setuptools()
         from setuptools import setup
     except ImportError:
         from distutils.core import setup
+
+extra = {}
+import sys
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+    extra['convert_2to3_doctests'] = ['doc/tutorial.rst']
 
 setup(
     name = 'ngram',
     version = '3.1',
     py_modules = ['ngram'],
     zip_safe = True,
-    author = 'Michael Albert, Graham Poulter',
+    author = 'Graham Poulter, Michael Albert',
     license = 'LGPL',
     url = 'http://packages.python.org/ngram',
+    download_url = 'http://pypi.python.org/pypi/ngram',
     description = "A set that retrieves members by N-Gram similarity to a query string.",
     keywords = "ngram string similarity",
+    test_suite = 'test_ngram',
+    **extra
 )
-
