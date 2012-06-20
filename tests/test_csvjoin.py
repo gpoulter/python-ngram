0e86f4bbdced6ff325649773fe8203888ab96e67
Graham Poulter <graham@mocality.com>
2012-06-20 21:48:34 +0200

    Contortions to make test_csvjoin pass under Py32

--- tests/test_csvjoin.py
+++ tests/test_csvjoin.py
@@ -9,24 +9,24 @@ class CsvjoinTests(unittest.TestCase):
 
     def test_csvjoin(self):
         left = tempfile.NamedTemporaryFile()
-        left.write('''ID,NAME\n1,Joe\n2,Kin\n3,ZAS''')
+        left.file.write(b'''ID,NAME\n1,Joe\n2,Kin\n3,ZAS''')
         left.flush()
         right = tempfile.NamedTemporaryFile()
-        right.write('''ID,NAME\nID,NAME\nA,Joe\nB,Jon\nC,Job\nD,Kim''')
+        right.file.write(b'''ID,NAME\nID,NAME\nA,Joe\nB,Jon\nC,Job\nD,Kim''')
         right.flush()
         output = tempfile.NamedTemporaryFile()
         subprocess.call([
             'csvjoin.py', '--titles', '-j', 'outer', '--minscore=0.24',
             '--count=5', '--warp=1.0',
             left.name, '1', right.name, '1', output.name])
-        result = '\n'.join(s.strip() for s in output.readlines())
+        result = b'\n'.join(s.strip() for s in output.readlines())
         correct = textwrap.dedent("""\
             ID,NAME,Rank,Similarity,ID,NAME
             1,Joe,1,1.0,A,Joe
             1,Joe,2,0.25,B,Jon
             1,Joe,3,0.25,C,Job
             2,Kin,1,0.25,D,Kim
-            3,ZAS""")
+            3,ZAS""").encode('ascii')
         self.assertEqual(result, correct)
 
 if __name__ == "__main__":
